2c4c40a7072818dc84e41e952d1fad1f9cd203f6
Jencer737 <2835775410101@ingenieria.usac.edu.gt>
2026-06-24 23:21:10 -0600

    202002141: se cambio el puesrto al voz-service

--- prccd-mvp/services/voz-service/src/config/env.js
+++ prccd-mvp/services/voz-service/src/config/env.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 3008,
+  port: process.env.PORT || 3009,
   nodeEnv: process.env.NODE_ENV || 'development',
   jwtSecret: process.env.JWT_SECRET || 'prccd_secret_2026',
   examenServiceUrl: process.env.EXAMEN_SERVICE_URL || 'http://examen-service:3003',
